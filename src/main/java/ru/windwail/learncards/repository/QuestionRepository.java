eb502f33c089d941ab42bece3c2ca52ac2ff8090
Afir Thes <afirthes@gmail.com>
2021-09-29 10:40:30 +0300

    search uppercase and lowercase

--- src/main/java/ru/windwail/learncards/repository/QuestionRepository.java
+++ src/main/java/ru/windwail/learncards/repository/QuestionRepository.java
@@ -20,13 +20,14 @@ public interface QuestionRepository extends PagingAndSortingRepository<Question,
 
     Page<Question> findQuestionByIdIn(List<Long> ids, Pageable page);
 
+    // Postgres specific
     @Query(value = "select q.id " +
             "from question as q " +
             "left join question_tag as qt on q.id=qt.question_id " +
             "left join tag as t on t.id=qt.tag_id " +
             "where " +
             "q.category_id = :categoryId and " +
-            "(t.name like %:q% or q.name like %:q%)", nativeQuery = true)
+            "(t.name ilike %:q% or q.name ilike %:q%)", nativeQuery = true)
     List<Long> filter(@Param("categoryId") Long categoryId, @Param("q") String q);
 
 }
